ec63e4d1cb6ea66b375c7b0f85e3cc78e6d6c327
DenisseDaugaard <U2478723@rts-365.dk>
2025-01-29 09:21:27 +0100

    ekstra opgave js del 2

--- Opgaver/Niveau1/02/script.js
+++ Opgaver/Niveau1/02/script.js
@@ -1,9 +1,22 @@
 let showResult = document.querySelector(".result")
 
-function div(x,y){
+function div(a,b){
 
-    return x+y;//scop variavble
+    return a/b;//scop variavble
 }
-let outerResult = add(3,4)
+let outerResult = div(4,3)
+console.log(outerResult);
+
+
+/* -------------begregne Forhold 2---------------- */
 
 showResult.textContent = outerResult
+function begregneForhold2 (a,b){
+
+    return a/b;//scop variavble
+}
+let outerResult2 = begregneForhold2(4,0)
+console.log(outerResult2);
+
+
+// showResult.textContent = outerResult
