8609c6db36e67f37a05ec26d5f4639c1715f5d6b
DenisseDaugaard <U2478723@rts-365.dk>
2025-01-30 08:47:50 +0100

    js_opgave 02/01

--- Opgaver/Niveau2/01/script.js
+++ Opgaver/Niveau2/01/script.js
@@ -1,1 +1,13 @@
+let buttons = document.querySelectorAll(".button")
+console.log(buttons);
 
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", changeColor);
+});
+
+function changeColor(event) {
+    buttons.forEach(btn => btn.style.backgroundColor = ""); // her all the buttons are the defult color
+
+    event.target.style.backgroundColor = "green"; // her we change the color with event.target
+}
