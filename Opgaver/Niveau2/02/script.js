9763223d23c8f0a796be56b33fc840f5a506e10c
DenisseDaugaard <U2478723@rts-365.dk>
2025-01-30 12:42:01 +0100

    opgave 02/02 færdig

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -2,14 +2,29 @@ let plusBtns = document.querySelectorAll(".item__plus__btn")
 let minusBtns = document.querySelectorAll(".item__minus__btn")
 
 
+
 plusBtns.forEach( plusBtnElm=> {
     plusBtnElm.addEventListener("click", function(event){
         if (event.target){
-            console.log(event.target.nextElementSibling)
+            //console.log(event.target.nextElementSibling)
             let inputAmount = event.target.nextElementSibling
             inputAmount.value = parseInt(inputAmount.value) + 1;
-        }
-     
+
+
+                //console.log(event.target.closest(".shop__item__description").querySelector(".price").textContent);
+            let price = event.target.closest(".shop__item__description").querySelector(".item__price").textContent
+
+            //console.log(parseFloat(price.replace("kr", "")));
+            let total = parseInt(parseFloat(price.replace("kr", "")))
+            //console.log(Boolean(isNaN(total)))
+            
+            //console.log(total*inputAmount.value);
+            //console.log(event.target.closest(".shop__item__description").querySelector(".total").textContent);
+            let totalPrice = event.target.closest(".shop__item__description").querySelector(".total")
+            
+            totalPrice.innerHTML = total*inputAmount.value + "kr"
+            }
+
     })
 });
 
@@ -19,11 +34,28 @@ minusBtns.forEach(minusBtnElmt => {
 
             console.log(event.target.previousElementSibling)
             let inputAmount = event.target.previousElementSibling
-            
+
             if(parseInt(inputAmount.value) > 0){
                 inputAmount.value = parseInt(inputAmount.value) -1 
+
+
+                  //console.log(event.target.closest(".shop__item__description").querySelector(".price").textContent);
+            let price = event.target.closest(".shop__item__description").querySelector(".item__price").textContent
+
+            //console.log(parseFloat(price.replace("kr", "")));
+            let total = parseInt(parseFloat(price.replace("kr", "")))
+            //console.log(Boolean(isNaN(total)))
+            
+            //console.log(total*inputAmount.value);
+            //console.log(event.target.closest(".shop__item__description").querySelector(".total").textContent);
+            let totalPrice = event.target.closest(".shop__item__description").querySelector(".total")
+            
+            totalPrice.innerHTML = total*inputAmount.value + "kr"
+             
             }
         }
 
     })
 });
+
+
